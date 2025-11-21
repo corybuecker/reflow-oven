9ef25c83fa5d427680db5ede0c61b5236d53de5d
Cory Buecker <email@corybuecker.com>
2025-11-21 05:52:27 -0600

    --wip-- [skip ci]

--- software/src/main.rs
+++ software/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod common;
+
+use core::future;
+
+use embassy_executor::Spawner;
+use esp_hal::{clock::CpuClock, timer::timg::TimerGroup};
+
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) -> ! {
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    future::pending().await
 }

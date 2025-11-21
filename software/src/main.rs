3720956b1239ed9a2c1465c914505ad48242d780
Cory Buecker <email@corybuecker.com>
2025-11-21 06:01:44 -0600

    --wip-- [skip ci]

--- software/src/main.rs
+++ software/src/main.rs
@@ -6,7 +6,11 @@ mod common;
 use core::future;
 
 use embassy_executor::Spawner;
-use esp_hal::{clock::CpuClock, timer::timg::TimerGroup};
+use esp_hal::{
+    clock::CpuClock,
+    spi::master::{Config, Spi},
+    timer::timg::TimerGroup,
+};
 
 #[esp_rtos::main]
 async fn main(_spawner: Spawner) -> ! {
@@ -20,6 +24,22 @@ async fn main(_spawner: Spawner) -> ! {
         esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
 
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    let spi = Spi::new(peripherals.SPI2, Config::default()).unwrap();
+    let mut spi = spi
+        .with_cs(peripherals.GPIO16)
+        .with_sck(peripherals.GPIO7)
+        .with_miso(peripherals.GPIO2);
+
+    let mut buffer: [u8; 64] = [0; 64];
+    match spi.read(&mut buffer) {
+        Ok(_) => {
+            defmt::error!("{}", &buffer);
+        }
+        Err(e) => {
+            defmt::error!("{}", e);
+        }
+    }
 
     future::pending().await
 }

2db0fae04bac3f947658a1db72ebb1c72da3454b
Cory Buecker <email@corybuecker.com>
2025-11-27 11:25:30 -0600

    feat: read temperature from SPI and adjust output pin (#7)

--- software/src/main.rs
+++ software/src/main.rs
@@ -7,12 +7,13 @@ mod profile;
 mod temperature;
 
 use crate::{
-    led::{create_channel, off, red_led},
+    led::{blue_led, create_channel, off, red_led},
     profile::Profile,
     temperature::Temperature,
 };
 use core::future;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_time::{Instant, Timer};
 use esp_hal::{
     Async, Blocking,
@@ -27,7 +28,7 @@ use static_cell::StaticCell;
 static TEMPERATURE: StaticCell<Temperature> = StaticCell::new();
 
 #[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
+async fn main(_spawner: Spawner) -> ! {
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -41,15 +42,12 @@ async fn main(spawner: Spawner) -> ! {
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
 
     let spi = Spi::new(peripherals.SPI2, Config::default()).unwrap();
-
     let spi = spi
         .with_cs(peripherals.GPIO4)
         .with_miso(peripherals.GPIO5)
         .with_sck(peripherals.GPIO6);
 
     let temperature = TEMPERATURE.init(Temperature::default());
-
-    let _ = spawner.spawn(read_temperature_wrapper(temperature, spi));
 
     let output = Output::new(
         peripherals.GPIO7,
@@ -58,16 +56,18 @@ async fn main(spawner: Spawner) -> ! {
     );
     let mut channel = create_channel(peripherals.RMT, peripherals.GPIO8).await;
 
-    Timer::after_millis(500).await;
-    let _ = off(&mut channel).await;
+    let _ = blue_led(&mut channel).await;
     Timer::after_secs(5).await;
 
-    let _ = spawner.spawn(adjust_temperature_wrapper(output, channel, temperature));
+    join(
+        read_temperature_wrapper(temperature, spi),
+        adjust_temperature_wrapper(output, channel, temperature),
+    )
+    .await;
 
     future::pending().await
 }
 
-#[embassy_executor::task]
 async fn read_temperature_wrapper(
     temperature: &'static Temperature,
     mut spi_peripheral: Spi<'static, Blocking>,
@@ -75,32 +75,56 @@ async fn read_temperature_wrapper(
     temperature.read_continuous(&mut spi_peripheral).await
 }
 
-#[embassy_executor::task]
 async fn adjust_temperature_wrapper(
     mut output: Output<'static>,
     mut channel: Channel<'static, Async, Tx>,
     temperature: &'static Temperature,
 ) -> () {
-    let profile = Profile::new();
+    let mut profile = Profile::new();
+
+    if temperature.current_reading() < profile.heatsoak_target {
+        while temperature.current_reading() < profile.heatsoak_target {
+            defmt::info!(
+                "heatsoak_current_temperature={}",
+                temperature.current_reading()
+            );
+
+            output.set_high();
+
+            #[allow(unused)]
+            red_led(&mut channel).await;
+
+            Timer::after_secs(1).await;
+        }
+        output.set_low();
+
+        #[allow(unused)]
+        off(&mut channel).await;
+    }
+
     let program_start = Instant::now();
 
     loop {
         let runtime = Instant::now() - program_start;
-        let desired_temperature = profile.get_target(runtime.as_millis() as f32 / 1000.0);
+        let runtime = runtime.as_millis() as f32 / 1000.0;
         let current_temperature = temperature.current_reading();
+        let desired_temperature = profile.desired_temperature(runtime);
+        let control_output = profile.control_output(runtime, current_temperature);
 
         defmt::info!(
-            "desired={} current={}",
+            "runtime={} desired_temperature={} current_temperature={} control_output={}",
+            runtime,
             desired_temperature,
-            current_temperature
+            current_temperature,
+            control_output.output
         );
 
-        if current_temperature > desired_temperature {
-            output.set_low();
-            let _ = off(&mut channel).await;
-        } else {
+        if control_output.output > 0.0 {
             output.set_high();
             let _ = red_led(&mut channel).await;
+        } else {
+            output.set_low();
+            let _ = off(&mut channel).await;
         }
 
         Timer::after_millis(50).await;
